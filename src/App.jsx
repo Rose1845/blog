e16f0b64fd155b92f5102f194d0690721a731071
Rose Odhiambo <odhiamborose466@gmail.com>
2022-11-24 12:22:07 +0300

    added the routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 
 import './App.css'
-
+import {BrowserRouter,Route,Routes}from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ArticlePage from './pages/ArticlePage'
+import ArticleList from './pages/ArticleList'
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <div className="App">
-      
-     
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/'element={<HomePage/>}/>
+    <Route path='/about'element={<AboutPage/>}/>
+    <Route path='/article:articleId'element={<ArticlePage/>}/>
+    <Route path='/articles'element={<ArticleList/>}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
